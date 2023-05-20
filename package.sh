0ed4cecd1d0d44962d37d5a14b09e09d6d3c8e12
Weijia <caivega@163.com>
2023-05-20 11:14:43 +0800

    update the package.sh

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 npm run build:wasm
 npm run build
+grep -rl '/assets' ./dist | sort | uniq | xargs perl -e"s/\/assets/\.\/assets/g" -pi
 rm -rf ./register.zip && cd dist/ && zip -r ../register.zip ./ && cd ../
